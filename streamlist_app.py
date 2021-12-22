6955d0a56f86cd17d00aa28a8db2007bc0012a8b
Tom Bresee <tbresee@umich.edu>
2021-12-22 16:19:45 -0600

    -

--- streamlist_app.py
+++ streamlist_app.py
@@ -28,12 +28,12 @@ st.set_page_config(
 # local_css(css_file)
 
 
-st.markdown("### 🎲 The Application")
+st.markdown("### My Application")
 
 st.markdown("This application is a Streamlit dashboard hosted on Heroku that can be used"
             "to explore the results from board game matches that I tracked over the last year.")
 
-st.markdown("**♟ General Statistics ♟**")
+st.markdown("**General Statistics **")
 
 st.markdown("* This gives a general overview of the data including"
             "frequency of games over time, most games played in a day, and longest break"
