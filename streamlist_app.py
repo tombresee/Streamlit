387f527047f124f1ff5947cbfafb4189385adb11
Tom Bresee <tbresee@umich.edu>
2021-12-22 16:14:10 -0600

    -

--- streamlist_app.py
+++ streamlist_app.py
@@ -7,6 +7,17 @@ import numpy as np
 
 import altair as alt
 
+st.markdown("### 🎲 The Application")
+st.markdown("This application is a Streamlit dashboard hosted on Heroku that can be used"
+            "to explore the results from board game matches that I tracked over the last year.")
+st.markdown("**♟ General Statistics ♟**")
+st.markdown("* This gives a general overview of the data including"
+            "frequency of games over time, most games played in a day, and longest break"
+            "between games.")
+
+
+            
+
 x = st.slider('Select a value')
 
 st.write(x, 'squared is', x * x)
