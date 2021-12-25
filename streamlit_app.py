e9d677db162dcc8296fe4d0e679b104520f5163c
Tom Bresee <tbresee@umich.edu>
2021-12-25 14:07:30 -0600

    -

--- streamlit_app.py
+++ streamlit_app.py
@@ -112,19 +112,20 @@ def cs_body():
 #---------------------------------------------------------------------------------------------------------
     # SECTION
     # charts 
-    col1.subheader('Display chart 1')
+    col1.subheader('Visulize Chart')
     col1.code('''
     import pandas as pd
     import numpy as np
     import altair as alt
-
-    df = pd.DataFrame(
-         np.random.randn(200, 3),
-         columns=['a', 'b', 'c'])
-
+    df = pd.DataFrame(np.random.randn(200, 3),
+                      columns=['a', 'b', 'c'])
     c = alt.Chart(df).mark_circle().encode(
-       x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
-    st.write(c)''')
+                           x='a', 
+                           y='b', 
+                           size='c',
+                           color='c',
+                           tooltip=['a', 'b', 'c'])
+    st.write(c)  # outputs chart''')
 #---------------------------------------------------------------------------------------------------------
     # SECTION
     # Display charts
