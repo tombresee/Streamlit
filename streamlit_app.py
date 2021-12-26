288a3f819b623391a5bebf7a039fa45ae35e4b31
Tom Bresee <tbresee@umich.edu>
2021-12-26 13:16:20 -0600

    -

--- streamlit_app.py
+++ streamlit_app.py
@@ -257,7 +257,7 @@ def cs_main_body():
     # >>> with st.expander('Expand'):
     # >>>     st.write('Juicy deets')
     # ''')
-    st.latex(r'''
+    col2.st.latex(r'''
      a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
      \sum_{k=0}^{n-1} ar^k =
      a \left(\frac{1-r^{n}}{1-r}\right)
